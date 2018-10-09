bf55d54a75234f06b91c0720b0b3964066b240b1
famoser <git@famoser.ch>
2018-10-09 18:20:21 +0200

    working angular setup

---
+++ WgWall/ClientApp/src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html'
+})
+export class TopBarComponent {
+}
--- WgWall/ClientApp/src/app/users/users.component.ts
+++ WgWall/ClientApp/src/app/users/users.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-counter-component',
-  templateUrl: './counter.component.html'
+  selector: 'app-users',
+  templateUrl: './users.component.html'
 })
-export class CounterComponent {
+export class UsersComponent {
   public currentCount = 0;
 
   public incrementCounter() {
