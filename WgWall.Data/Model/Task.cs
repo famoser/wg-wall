0c2820bbcb497f00f54f0abb1b093fb5837a4cd0
Florian Moser <products@famoser.ch>
2018-10-17 12:02:55 +0200

    adding migrations

--- WgWall.Data/Model/Task.cs
+++ WgWall.Data/Model/Task.cs
@@ -8,10 +8,10 @@ namespace WgWall.Data.Model
         public DateTime ActivatedAt { get; set; }
 
         public int TaskTemplateId { get; set; }
-        public TaskTemplate TaskTemplate { get; set; }
+        public virtual TaskTemplate TaskTemplate { get; set; }
 
         public int? DoneById { get; set; }
-        public FrontendUser DoneBy { get; set; }
+        public virtual FrontendUser DoneBy { get; set; }
 
         public static Task Create(TaskTemplate taskTemplate, FrontendUser createdBy)
         {
