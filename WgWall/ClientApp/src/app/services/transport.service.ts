e7980fd898fa37bd4096df38174b9f67bbc49889
Florian Moser <products@famoser.ch>
2018-10-25 13:50:52 +0200

    adding reload to station board2

--- WgWall/ClientApp/src/app/services/transport.service.ts
+++ WgWall/ClientApp/src/app/services/transport.service.ts
@@ -1,29 +1,33 @@
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Vehicle } from '../models/vehicle';
+import { ReloadService } from './reload.service';
 
 @Injectable({ providedIn: 'root' })
 export class TransportService {
   private baseApiUrl = "https://transport.opendata.ch/v1/stationboard?limit=4&station="
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private reload: ReloadService) {
   }
 
   public get(stationName: string): Observable<Vehicle[]> {
-    return this.http.get(this.baseApiUrl + stationName).pipe(map(transportJson => {
-      let vehicles = [];
-      transportJson["stationboard"].forEach(element => {
-        let vehicle = new Vehicle();
-        vehicle.name = element["category"] + " " + element["number"];
-        vehicle.departureTime = new Date(element["stop"]["departure"]);
-        vehicle.to = element["to"];
-        vehicles.push(vehicle);
-      });
-      return vehicles;
-    }));
+    return this.reload.reloadObservable.pipe(
+      switchMap(() => {
+        return this.http.get(this.baseApiUrl + stationName).pipe(map(transportJson => {
+          let vehicles = [];
+          transportJson["stationboard"].forEach(element => {
+            let vehicle = new Vehicle();
+            vehicle.name = element["category"] + " " + element["number"];
+            vehicle.departureTime = new Date(element["stop"]["departure"]);
+            vehicle.to = element["to"];
+            vehicles.push(vehicle);
+          });
+          return vehicles;
+        }));
+      }));
   };
 }
